377cfa8372269fcf5cc996188c47a30a9fb34830
KalevRK <KalevRK@gmail.com>
2015-02-04 18:00:57 -0800

    Implement stack in the functional-shared pattern.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.count = 0;
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
+
+// push
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+// pop
+stackMethods.pop = function() {
+  this.count && this.count--;
+  var value = this.storage[this.count];
+  delete this.storage[this.count];
+  return value;
+};
+
+// size
+stackMethods.size = function() {
+  return this.count;
+};
 
 
