437d0f0e5db231f060acde4ad677f8c83475013d
KalevRK <KalevRK@gmail.com>
2015-02-04 16:42:20 -0800

    Complete functional stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,23 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var head = 0;
+  var tail = 0;
+  //debugger;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[tail] = value;
+    tail++;
   };
 
   someInstance.dequeue = function(){
+    (tail-head) && head++;
+    return storage[--head];
   };
 
   someInstance.size = function(){
+    return tail-head;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,23 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function(){
+    count && count--;
+    var results = storage[count];
+    delete storage[count];
+    return results;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
