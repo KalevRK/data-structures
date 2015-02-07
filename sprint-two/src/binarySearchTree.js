d7d2001248e6a35cc09214cfbd28bec4b14fb0b2
KalevRK <KalevRK@gmail.com>
2015-02-06 16:42:36 -0800

    In progress BST implementation.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,55 @@
 var BinarySearchTree = function(value){
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  if (value <= this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
 
+BinarySearchTree.prototype.contains = function(value) {
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (this.left) {
+      return this.left.contains(value);
+    }
+  } else {
+    if (this.right) {
+      return this.right.contains(value);
+    }
+  }
+  return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+
+};
+
+//worth some hi-chews on completion
+BinarySearchTree.prototype.breadthFirstLog = function(callback){
+
+}
+
+//worth lots of hi-chews
+BinarySearchTree.prototype.rebalanceTree = function(){
+
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
